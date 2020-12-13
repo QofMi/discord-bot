a2728af09bf4f516838e3875a211c7ec03121c58
QofMi <avatar11132@gmail.com>
2020-12-13 20:58:08 +0700

    update commands

--- bot/main.py
+++ bot/main.py
@@ -63,14 +63,28 @@ async def help(ctx):
 
     help.add_field(name='qmblog.ru', value='Да да, рекламирую свой сайт.', inline=True)
 
-    help.add_field(name='{}join'.format(PREFIX), value='Эта команда добавляет бота в голосовой канал', inline=False)
+    help.add_field(name='{}join'.format(PREFIX), value='Эта команда добавляет бота в голосовой канал.', inline=False)
 
-    help.add_field(name='{}leave'.format(PREFIX), value='Эта команда удаляет бота из голосового канала', inline=False)
+    help.add_field(name='{}leave'.format(PREFIX), value='Эта команда удаляет бота из голосового канала.', inline=False)
+
+    help.add_field(name='{}queue'.format(PREFIX), value='Эта команда добавляет трек в очередь.', inline=False)
+
+    help.add_field(name='{}remove'.format(PREFIX), value='Эта команда удаляет трек из списка.', inline=False)
+
+    help.add_field(name='{}play'.format(PREFIX), value='Команда для проигрывания трека.', inline=False)
+
+    help.add_field(name='{}pause'.format(PREFIX), value='Эта команда ставит трек на паузу.', inline=False)
+
+    help.add_field(name='{}resume'.format(PREFIX), value='Эта команда возобновляет трек.', inline=False)
+
+    help.add_field(name='{}view'.format(PREFIX), value='Эта команда показывает список треков в очереди.', inline=False)
+
+    help.add_field(name='{}stop'.format(PREFIX), value='Эта команда прекращает проигрывание музыки.', inline=False)
 
     await ctx.send(embed=help)
 
 
-@bot.command(name='join', help='Эта команда добавляет бота в голосовой канал')
+@bot.command(name='join', help='Эта команда добавляет бота в голосовой канал.')
 async def join(ctx):
     if not ctx.message.author.voice:
         await ctx.send("Голосовой канал пуст")
@@ -80,70 +94,73 @@ async def join(ctx):
     await channel.connect()
 
 
-@bot.command(name='leave', help='Эта команда удаляет бота из голосового канала')
+@bot.command(name='leave', help='Эта команда удаляет бота из голосового канала.')
 async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
     await voice_client.disconnect()
 
 
+@bot.command(name='queue', help='Эта команда добавляет трек в очередь.')
+async def queue_(ctx, url):
+    global queue
+    queue.append(url)
+    await ctx.send(f'`{url}` трек добавлен в очередь!')
 
 
-# @bot.command(name='queue', help='This command adds a song to the queue')
-# async def queue_(ctx, url):
-#     global queue
-#
-#     queue.append(url)
-#     await ctx.send(f'`{url}` added to queue!')
-#
-# @bot.command(name='remove', help='This command removes an item from the list')
-# async def remove(ctx, number):
-#     global queue
-#
-#     try:
-#         del(queue[int(number)])
-#         await ctx.send(f'Your queue is now `{queue}!`')
-#
-#     except:
-#         await ctx.send('Your queue is either **empty** or the index is **out of range**')
-#
-# @bot.command(name='play', help='This command plays songs')
-# async def play(ctx):
-#     global queue
-#
-#     server = ctx.message.guild
-#     voice_channel = server.voice_client
-#
-#     async with ctx.typing():
-#         player = await YTDLSource.from_url(queue[0], loop=bot.loop)
-#         voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-#
-#     await ctx.send('**Now playing:** {}'.format(player.title))
-#     del(queue[0])
-#
-# @bot.command(name='pause', help='This command pauses the song')
-# async def pause(ctx):
-#     server = ctx.message.guild
-#     voice_channel = server.voice_client
-#
-#     voice_channel.pause()
-#
-# @bot.command(name='resume', help='This command resumes the song!')
-# async def resume(ctx):
-#     server = ctx.message.guild
-#     voice_channel = server.voice_client
-#
-#     voice_channel.resume()
-#
-# @bot.command(name='view', help='This command shows the queue')
-# async def view(ctx):
-#     await ctx.send(f'Your queue is now `{queue}!`')
-#
-# @bot.command(name='stop', help='This command stops the song!')
-# async def stop(ctx):
-#     server = ctx.message.guild
-#     voice_channel = server.voice_client
-#
-#     voice_channel.stop()
+@bot.command(name='remove', help='Эта команда удаляет трек из списка.')
+async def remove(ctx, number):
+    global queue
+
+    try:
+        del(queue[int(number)])
+        await ctx.send(f'ЫЫЫЫЫ`{queue}!`')
+
+    except:
+        await ctx.send('ПАШОЛ НАХУЙ')
+
+
+@bot.command(name='play', help='Команда для проигрывания трека.')
+async def play(ctx):
+    global queue
+
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(queue[0], loop=bot.loop)
+        voice_channel.play(player, after=lambda e: print('Ля, ну ты чаго наделал: %s' % e) if e else None)
+
+    await ctx.send('**Сейчас играет:** {}'.format(player.title))
+    del(queue[0])
+
+
+@bot.command(name='pause', help='Эта команда ставит трек на паузу.')
+async def pause(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.pause()
+
+
+@bot.command(name='resume', help='Эта команда возобновляет трек.')
+async def resume(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.resume()
+
+
+@bot.command(name='view', help='Эта команда показывает список треков в очереди.')
+async def view(ctx):
+    await ctx.send(f'Треки в списке `{queue}!`')
+
+
+@bot.command(name='stop', help='Эта команда прекращает проигрывание музыки!')
+async def stop(ctx):
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    voice_channel.stop()
 
 
 bot.run(BOT_TOKEN)
