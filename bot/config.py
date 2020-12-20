d1a9fde0eec6ee8fa4bc39888e359e1e9a6d236b
QofMi <avatar11132@gmail.com>
2020-12-20 16:18:51 +0700

    clear config file

--- bot/config.py
+++ bot/config.py
@@ -1,30 +1,15 @@
-import os
-import environ
+BOT_TOKEN = ''
 
-
-# Build paths inside the project
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Environ
-env_file = os.path.join(BASE_DIR, '.env')
-environ.Env.read_env(env_file)
-
-
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-
-CHANNEL_ID = 787269607198818316
+CHANNEL_ID =
 
 PREFIX = '/'
 
 MAIN_COLOR = 0xd3597e
 
 BOT_STATUS = [
-'Пошел срать!',
-'Кастую гейскую магию!!!'
+'',
 ]
 
 HELLO_MESSAGES = [
-'Hello1',
-'Hello2',
-'Hello3'
+'',
 ]
