d8745381cf556cc36ec7b414d167c898d5a98181
QofMi <avatar11132@gmail.com>
2021-01-24 18:53:45 +0700

    added on_message event

--- bot/extensions/events.py
+++ bot/extensions/events.py
@@ -5,13 +5,37 @@ from discord.ext import commands, tasks
 # Модуль logging
 import logging
 
+# utils
+from ..utils import _random_choice
 
-# class Events(commands.Cog):
-#
-#     def __init__(self, bot):
-#         self.bot = bot
-#
-#
-#
-# def setup(bot):
-#     bot.add_cog(Events(bot))
+
+TRIGGER_MESSAGE = [
+'',
+]
+
+MEM_MESSAGE = [
+'',
+]
+
+
+async def send_message_on_trigger(message, trigger_message: None, message_list: None):
+    for i in trigger_message:
+        if i in message.content.lower():
+            await message.channel.send(_random_choice(list=message_list))
+
+
+class Events(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot:
+            return
+
+        await send_message_on_trigger(message, trigger_message=TRIGGER_MESSAGE, message_list=MEM_MESSAGE)
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
