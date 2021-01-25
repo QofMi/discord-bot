c28c695cd3e88645506237f1b3cb647793d54eca
QofMi <avatar11132@gmail.com>
2021-01-25 16:57:39 +0700

    added dynamic generating custom help command

---
+++ bot/commands.py
@@ -0,0 +1,64 @@
+# Файл с настройками
+from .config import *
+
+
+COMMANDS = {}
+
+
+MUSIC = {
+    "join": {
+        "name": "join",
+        "help": "Эта команда добавляет бота в голосовой канал.",
+        "aliases": "j",
+    },
+    "leave": {
+        "name": "leave",
+        "help": "Эта команда удаляет бота из голосового канала.",
+        "aliases": "l",
+    },
+    "queue": {
+        "name": "queue",
+        "help": "Эта команда добавляет трек в очередь.",
+        "aliases": "q",
+    },
+    "remove": {
+        "name": "remove",
+        "help": "Эта команда удаляет трек из списка.",
+        "aliases": "r",
+    },
+    "play": {
+        "name": "play",
+        "help": "Команда для проигрывания трека.",
+        "aliases": "p",
+    },
+    "pause": {
+        "name": "pause",
+        "help": "Эта команда ставит трек на паузу.",
+        "aliases": "ps",
+    },
+    "resume": {
+        "name": "resume",
+        "help": "Эта команда возобновляет трек.",
+        "aliases": "rs",
+    },
+    "view": {
+        "name": "view",
+        "help": "Эта команда показывает список треков в очереди.",
+        "aliases": "v",
+    },
+    "stop": {
+        "name": "stop",
+        "help": "Эта команда прекращает проигрывание музыки!",
+        "aliases": "st",
+    },
+}
+
+
+def gen_commands() -> None:
+    if 'bot.extensions.music' in EXTENSIONS:
+        COMMANDS.update(MUSIC)
+    if 'bot.extensions.genpass' in EXTENSIONS:
+        COMMANDS.update(GEN_PASS)
+
+
+gen_commands()
--- bot/extensions/help.py
+++ bot/extensions/help.py
@@ -5,6 +5,12 @@ from discord.ext import commands
 # Файл с настройками
 from ..config import *
 
+# Модуль logging
+import logging
+
+# commands
+from ..commands import COMMANDS
+
 
 class Help(commands.Cog):
 
@@ -19,21 +25,12 @@ class Help(commands.Cog):
         help = discord.Embed(title='Информация о доступных командах.', color=MAIN_COLOR)
         help.add_field(name='QofMi', value='Разработчик этой хуйни', inline=True)
         help.add_field(name='qmblog.ru', value='Да да, рекламирую свой сайт.', inline=True)
-
-        if 'bot.extensions.music' in EXTENSIONS:
-            help.add_field(name='==============================', value='--------------Музыка--------------', inline=False)
-            help.add_field(name=f'{PREFIX}join или {PREFIX}j', value='Эта команда добавляет бота в голосовой канал.', inline=False)
-            help.add_field(name=f'{PREFIX}leave или {PREFIX}l', value='Эта команда удаляет бота из голосового канала.', inline=False)
-            help.add_field(name=f'{PREFIX}queue или {PREFIX}q', value='Эта команда добавляет трек в очередь.', inline=False)
-            help.add_field(name=f'{PREFIX}remove или {PREFIX}r', value='Эта команда удаляет трек из списка.', inline=False)
-            help.add_field(name=f'{PREFIX}play или {PREFIX}p', value='Команда для проигрывания трека.', inline=False)
-            help.add_field(name=f'{PREFIX}pause', value='Эта команда ставит трек на паузу.', inline=False)
-            help.add_field(name=f'{PREFIX}resume', value='Эта команда возобновляет трек.', inline=False)
-            help.add_field(name=f'{PREFIX}view или {PREFIX}v', value='Эта команда показывает список треков в очереди.', inline=False)
-            help.add_field(name=f'{PREFIX}stop', value='Эта команда прекращает проигрывание музыки.', inline=False)
-        else:
-            help.add_field(name='つ ◕_◕ つ 404', value='В данный момент нет действующих команд!', inline=False)
-
+        try:
+            for i in COMMANDS:
+                help.add_field(name=f'{PREFIX}{COMMANDS[i]["name"]} или {PREFIX}{COMMANDS[i]["aliases"]}', value=COMMANDS[i]["help"], inline=False)
+        except Exception as error:
+            logging.error(f"ERROR -------> {error}")
+    
         await ctx.send(embed=help)
 
 
